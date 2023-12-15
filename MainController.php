57033ae9c9def25b8e55d5cc1a3914e68f5190dd
Stefan Georg Rath <stefan@sg6.eu>
2023-12-15 11:00:47 +0100

    Create MainController.php

---
+++ MainController.php
@@ -0,0 +1,8 @@
+<?php
+
+function main() {
+  // function to create a new file
+  // the file should have a unique name, and the content of the function is the current timestamp (UNIX format).
+}
+
+main();
